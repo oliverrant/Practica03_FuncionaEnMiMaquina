52d9d3ba373fbc76399f5b85a33d234a383297d3
Valeria Ca <12345678+czvaleria@users.noreply.github.com>
2026-03-21 21:55:59 -0600

    Descripcion

---
+++ practica03/Practica03_FuncionaEnMiMaquina/src/ValleDragon.java
@@ -0,0 +1,14 @@
+/**
+ * Campo de entrenamiento de tipo Valle del Dragón.
+ * Extiende la clase abstracta CampoEntrenamiento.
+ * 
+ */
+public class ValleDragon extends CampoEntrenamiento {
+    /**
+     * Constructor de la clase ValleDragon.
+     * Inicializa el campo con nombre y descripción predefinidos.
+     */
+    public ValleDragon() {
+        super("Valle del Dragón", "Residencia de la esposa del Burro de Shrek");
+    }
+}
